27f7e954a483d70a59abf0c3c6f904070a413e93
yusuf <yusufgad24@gmail.com>
2024-12-06 21:37:29 +0300

    Make sidebar sticky at the bottom on larger screens

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,6 @@
 function Sidebar() {
   return (
-    <div className="h-24 lg:h-auto lg:max-w-80 lg:w-80 bg-dark order-1 lg:-order-1 rounded-t-xl lg:rounded-r-xl lg:rounded-t-none lg:rounded-tr-xl">
+    <div className="sticky bottom-0 lg:inset-0 h-24 lg:h-auto lg:max-w-80 lg:w-80 bg-dark order-1 lg:-order-1 rounded-t-xl lg:rounded-r-xl lg:rounded-t-none lg:rounded-tr-xl">
       sidebar
     </div>
   );

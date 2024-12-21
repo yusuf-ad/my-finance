af57f9ffbb157d0c86b7f83bbac34643ce618d0c
yusuf <yusufgad24@gmail.com>
2024-12-21 21:43:58 +0300

    Create budgets page layout structure

--- app/(dashboard)/budgets/page.tsx
+++ app/(dashboard)/budgets/page.tsx
@@ -18,12 +18,28 @@ async function BudgetsPage() {
         <BudgetsModal />
       </header>
 
-      <section>
+      <section className="my-8">
         {budgets.length === 0 && (
           <p className="text-gray-400 font-bold mt-8">
             You haven&apos;t created a budget yet.
           </p>
         )}
+
+        <div className="flex flex-col lg:flex-row gap-4">
+          <div className="flex-1 lg:max-w-sm bg-white p-6 "></div>
+          <div className="flex-grow bg-white p-6 ">
+            <h2 className="text-gray-900 font-bold text-lg">Budgets</h2>
+            <div className="mt-4">
+              {budgets.map((budget) => (
+                <div key={budget.id} className="flex justify-between">
+                  <p>{budget.category}</p>
+                  <p>{budget.theme}</p>
+                  <p>{budget.maxSpend}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );

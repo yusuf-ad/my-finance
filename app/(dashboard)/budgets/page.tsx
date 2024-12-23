93b996803c6c8ea860ec61efa7f4fdc319017a61
yusuf <yusufgad24@gmail.com>
2024-12-23 14:18:14 +0300

    Add budgets summary and chart components with styling enhancements

--- app/(dashboard)/budgets/page.tsx
+++ app/(dashboard)/budgets/page.tsx
@@ -1,9 +1,7 @@
 import BudgetCard from "@/components/budget-card";
 import BudgetsModal from "@/components/budgets-modal";
-import { CaretRight, Dots } from "@/components/icons";
-import { parseTheme } from "@/lib/utils";
+import BudgetsSummary from "@/components/budgets-summary";
 import { getBudgets } from "@/server/actions/budget";
-import Link from "next/link";
 
 async function BudgetsPage() {
   const res = await getBudgets();
@@ -29,48 +27,13 @@ async function BudgetsPage() {
           </p>
         )}
 
-        <div className="flex flex-col items-start lg:flex-row gap-4">
-          <div className="flex-1 lg:max-w-sm bg-white p-6 ">
-            <div>
-              <h3 className="text-gray-900 font-semibold mb-2 tracking-wide">
-                Spending Summary
-              </h3>
-
-              <ul>
-                {budgets.map((budget) => {
-                  const { code } = parseTheme(budget.theme);
-
-                  return (
-                    <li
-                      key={budget.id}
-                      className="flex justify-between items-center py-4 border-b last:border-b-0"
-                    >
-                      <div className="flex gap-2 items-center">
-                        <div
-                          className="w-[2px] h-5"
-                          style={{ backgroundColor: code }}
-                        ></div>
-                        <h4 className="capitalize text-gray-500 text-sm">
-                          {budget.category}
-                        </h4>
-                      </div>
-                      <p className="font-semibold">
-                        ${Number(0).toFixed(2)}{" "}
-                        <span className="text-gray-400 font-medium text-sm">
-                          of ${budget.maxSpend.toFixed(2)}
-                        </span>
-                      </p>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
+        <div className="flex flex-col lg:items-start lg:flex-row gap-4">
+          <BudgetsSummary budgets={budgets} />
 
           <ul className="flex-grow space-y-4">
-            {budgets.map((budget) => {
-              return <BudgetCard budget={budget} key={budget.id} />;
-            })}
+            {budgets.map((budget) => (
+              <BudgetCard budget={budget} key={budget.id} />
+            ))}
           </ul>
         </div>
       </section>
---
+++ components/budgets-summary.tsx
@@ -0,0 +1,48 @@
+import { parseTheme } from "@/lib/utils";
+import { Budget } from "@/server/actions/budget";
+import BudgetsChart from "./budgets-chart";
+
+function BudgetsSummary({ budgets }: { budgets: Budget[] }) {
+  return (
+    <div className="flex-1 lg:max-w-sm bg-white p-6 ">
+      <div>
+        <BudgetsChart />
+      </div>
+
+      <h3 className="text-gray-900 font-semibold mb-2 tracking-wide">
+        Spending Summary
+      </h3>
+
+      <ul>
+        {budgets.map((budget) => {
+          const { code } = parseTheme(budget.theme);
+
+          return (
+            <li
+              key={budget.id}
+              className="flex justify-between items-center py-4 border-b last:border-b-0"
+            >
+              <div className="flex gap-2 items-center">
+                <div
+                  className="w-[2px] h-5"
+                  style={{ backgroundColor: code }}
+                ></div>
+                <h4 className="capitalize text-gray-500 text-sm">
+                  {budget.category}
+                </h4>
+              </div>
+              <p className="font-semibold">
+                ${Number(0).toFixed(2)}{" "}
+                <span className="text-gray-400 font-medium text-sm">
+                  of ${budget.maxSpend.toFixed(2)}
+                </span>
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default BudgetsSummary;

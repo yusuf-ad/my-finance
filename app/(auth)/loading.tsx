86341031c65abfdafb2136f0273b9a1e86ccc400
yusuf <yusufgad24@gmail.com>
2025-01-09 17:46:35 +0300

    Add Loading component for auth pages

---
+++ app/(auth)/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from "lucide-react";
+
+function Loading() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Loader2 className="animate-spin" size={50} />
+    </div>
+  );
+}
+
+export default Loading;

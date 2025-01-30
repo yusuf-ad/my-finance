5444c5f5e4fa555fc9193124db4f9e931eea2daf
yusuf <yusufgad24@gmail.com>
2025-01-30 09:42:20 +0300

    Add portfolio-info to signup page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,4 +1,5 @@
 import SignupForm from "@/components/signup/signup-form";
+import { PortfolioInfo } from "@/components/portfolio-info";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -11,18 +12,22 @@ export const metadata: Metadata = {
 function SignupPage() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="bg-white py-6 pb-0 px-6 max-w-lg w-[512px] rounded-lg">
-        <h1 className="text-gray-900 text-3xl font-bold mb-8">Sign up</h1>
-
-        <SignupForm />
-
-        <Link
-          href="/login"
-          className="flex justify-center my-8 text-sm text-gray-500"
-        >
-          Already have an account?{" "}
-          <span className="ml-2 underline font-bold text-gray-900"> Login</span>
-        </Link>
+      <div className="w-[512px] max-w-lg">
+        <div className="bg-white py-6 pb-3 px-6 w-full rounded-lg">
+          <h1 className="text-gray-900 text-3xl font-bold mb-8">Sign up</h1>
+          <SignupForm />
+          <Link
+            href="/login"
+            className="flex justify-center my-8 text-sm text-gray-500"
+          >
+            Already have an account?{" "}
+            <span className="ml-2 underline font-bold text-gray-900">
+              {" "}
+              Login
+            </span>
+          </Link>
+        </div>
+        <PortfolioInfo />
       </div>
     </div>
   );
